a03aafb1614cbf4adce59aada91f4ca2de734231
drew <85300755+dahroo@users.noreply.github.com>
2024-09-02 02:27:10 -0400

    Vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './app/App'
 import './index.css'
+import { injectSpeedInsights } from '@vercel/speed-insights'
+
+injectSpeedInsights();
 
 createRoot(document.getElementById('root')!).render(
     <App />

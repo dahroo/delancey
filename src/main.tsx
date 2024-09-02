56c57fb485281bdcb25539f2422cd6969fbb184a
drew <85300755+dahroo@users.noreply.github.com>
2024-09-02 10:57:48 -0400

    Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-
+import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import { createRoot } from 'react-dom/client'
 import App from './app/App'
 import './index.css'
-import { injectSpeedInsights } from '@vercel/speed-insights'
 
+
+inject();
 injectSpeedInsights();
 
 createRoot(document.getElementById('root')!).render(

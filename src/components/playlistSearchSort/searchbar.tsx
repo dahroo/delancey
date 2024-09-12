daa4b58a3d905eea0766d01be1c6b946c75a1d61
drew <85300755+dahroo@users.noreply.github.com>
2024-09-12 00:50:29 -0400

    improved search

--- src/components/playlistSearchSort/searchbar.tsx
+++ src/components/playlistSearchSort/searchbar.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FaSearch } from 'react-icons/fa';
 
 interface SearchBarProps {
   value: string;
@@ -8,17 +7,14 @@ interface SearchBarProps {
 
 const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => {
   return (
-    <div className='relative w-full'>
+    <div className='w-full flex justify-center items-center'>
       <input
         type="text"
         placeholder="search your playlists..."
         value={value}
-        className="w-full focus:outline-none focus:ring-0 pl-7"
+        className="flex flex-grow min-w-[400px] m-3 text-xl rounded-full bg-black text-white placeholder-gray-400 p-2 focus:outline-none focus:ring-0 pl-5"
         onChange={(e) => onChange(e.target.value)}
       /> 
-      <div className="absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none">
-        <FaSearch />
-      </div> 
     </div>
     
   );
